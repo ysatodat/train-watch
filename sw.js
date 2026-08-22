5da0a749e2e39584ce5b3e533d246db2abf4a42d
Yoshiki Satodate <89145084+ysatodat@users.noreply.github.com>
2026-08-23 00:21:13 +0900

    Refresh cache for moments release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='densha-kuruyo-v14';
+const CACHE='densha-kuruyo-v15';
 const CORE=['./','./index.html','./styles.css','./mobile-fixes.css','./native-ui.css','./brand-refresh.css','./moments.css','./dialog-controller.js','./train-engine.js','./app.js','./microinteractions.js','./moments-interactions.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
